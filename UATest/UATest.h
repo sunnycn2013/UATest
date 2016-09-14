32e05528b8d9f825429ad8a7fa802eb64187e413
sunny <389878212@qq.com>
2016-09-14 16:55:12 +0800

    add

---
+++ UATest/UATest.h
@@ -0,0 +1,14 @@
+//
+//  UATest.h
+//  UATest
+//
+//  Created by guochaoyang on 16/9/14.
+//  Copyright © 2016年 guochaoyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UATest : NSObject
+- (void)testLog;
+
+@end
---
+++ UATest/UATest.m
@@ -0,0 +1,17 @@
+//
+//  UATest.m
+//  UATest
+//
+//  Created by guochaoyang on 16/9/14.
+//  Copyright © 2016年 guochaoyang. All rights reserved.
+//
+
+#import "UATest.h"
+
+@implementation UATest
+- (void)testLog
+{
+    NSLog(@"test");
+}
+
+@end
